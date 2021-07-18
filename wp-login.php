4979d344ff31bdc6c889f6ffdc7b4aa23ee3afc0
nordexdoors <nick+nordexdoors@kavadias.org>
2021-07-18 17:44:39 +1000

    leaving domain name as is

--- wp-login.php
+++ wp-login.php
@@ -9,24 +9,24 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Log In &lsaquo; Nordex Doors &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='/wp-//css/dashicons.min.css?ver=5.2' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='/wp-//css/buttons.min.css?ver=5.2' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='/wp-admin/css/forms.min.css?ver=5.2' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='/wp-admin/css/l10n.min.css?ver=5.2' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='/wp-admin/css/login.min.css?ver=5.2' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='http://www.nordexdoors.com.au/wp-includes/css/dashicons.min.css?ver=5.2' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='http://www.nordexdoors.com.au/wp-includes/css/buttons.min.css?ver=5.2' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='http://www.nordexdoors.com.au/wp-admin/css/forms.min.css?ver=5.2' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='http://www.nordexdoors.com.au/wp-admin/css/l10n.min.css?ver=5.2' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='http://www.nordexdoors.com.au/wp-admin/css/login.min.css?ver=5.2' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
-	<link rel="icon" href="/wp-content/uploads/2018/07/cropped-Logo-1-32x32.jpg" sizes="32x32" />
-<link rel="icon" href="/wp-content/uploads/2018/07/cropped-Logo-1-192x192.jpg" sizes="192x192" />
-<link rel="apple-touch-icon-precomposed" href="/wp-content/uploads/2018/07/cropped-Logo-1-180x180.jpg" />
-<meta name="msapplication-TileImage" content="/wp-content/uploads/2018/07/cropped-Logo-1-270x270.jpg" />
+	<link rel="icon" href="http://www.nordexdoors.com.au/wp-content/uploads/2018/07/cropped-Logo-1-32x32.jpg" sizes="32x32" />
+<link rel="icon" href="http://www.nordexdoors.com.au/wp-content/uploads/2018/07/cropped-Logo-1-192x192.jpg" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="http://www.nordexdoors.com.au/wp-content/uploads/2018/07/cropped-Logo-1-180x180.jpg" />
+<meta name="msapplication-TileImage" content="http://www.nordexdoors.com.au/wp-content/uploads/2018/07/cropped-Logo-1-270x270.jpg" />
 	</head>
 	<body class="login login-action-login wp-core-ui  locale-en-us">
 		<div id="login">
 		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
 	
-	<form name="loginform" id="loginform" action="/wp-login.php" method="post">
+	<form name="loginform" id="loginform" action="http://www.nordexdoors.com.au/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Username or Email Address<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
@@ -38,13 +38,13 @@
 			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-				<input type="hidden" name="redirect_to" value="/wp-admin/" />
+				<input type="hidden" name="redirect_to" value="http://www.nordexdoors.com.au/wp-admin/" />
 					<input type="hidden" name="testcookie" value="1" />
 	</p>
 	</form>
 
 			<p id="nav">
-					<a href="/wp-login.php?action=lostpassword">Lost your password?</a>
+					<a href="http://www.nordexdoors.com.au/wp-login.php?action=lostpassword">Lost your password?</a>
 				</p>
 	
 	<script type="text/javascript">
@@ -61,7 +61,7 @@
 			if(typeof wpOnload=='function')wpOnload();
 			</script>
 
-			<p id="backtoblog"><a href="/">
+			<p id="backtoblog"><a href="http://www.nordexdoors.com.au/">
 		&larr; Back to Nordex Doors	</a></p>
 			
 	</div>
